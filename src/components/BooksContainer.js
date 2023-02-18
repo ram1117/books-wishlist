ad872478766e0e74e8ad76c0535a2e1f306838bb
ram kumar karuppusamy <ramkumark1@outlook.com>
2023-02-18 10:18:18 +0530

    Display books list based on search key word. display book details in popup

--- src/components/BooksContainer.js
+++ src/components/BooksContainer.js
@@ -1,22 +1,26 @@
 import { PropTypes } from 'prop-types';
 import { useEffect, useState } from 'react';
+import './BooksContainer.css';
+import Modal from './Modal';
 
-const BOOK_URL = 'https://gutendex.com//books?search=';
+const BASE_URL = 'https://gutendex.com//books?search=';
 const BooksContainer = ({ input }) => {
   const [booksData, setBooksData] = useState([]);
+  const [toShowModal, setToShowModal] = useState(false);
+  const [bId, setbId] = useState('');
+
   BooksContainer.defaultProps = { input: '' };
   BooksContainer.propTypes = { input: PropTypes.string };
 
   const fetchFromApi = async (inputVal) => {
     if (input !== '') {
-      const response = await fetch(new URL(BOOK_URL + inputVal));
+      const response = await fetch(new URL(BASE_URL + inputVal));
       const books = await response.json();
       const newArr = [];
       books.results.forEach((element) => {
         const { id, title, authors } = element;
         newArr.push({ id, title, authors });
       });
-      console.log(newArr);
       setBooksData(newArr);
     }
   };
@@ -25,21 +29,26 @@ const BooksContainer = ({ input }) => {
     fetchFromApi(input);
   }, [input]);
 
+  const showModal = (e) => {
+    setToShowModal(true);
+    setbId(e.target.id);
+  };
+
   return (
     <div>
       <h1>Books list</h1>
       <div className="books-container">
         {booksData.map((book) => (
-          <>
-            <p key={book.id}>
-              {book.id}
+          <div className="book-tile" key={book.id}>
+            <div className="book-details">
               {book.title}
-            </p>
+              <button id={book.id} onClick={showModal} type="button">details..</button>
+            </div>
             <hr />
-          </>
+          </div>
         ))}
       </div>
-
+      {toShowModal && <Modal bookId={bId} funcModal={setToShowModal} />}
     </div>
   );
 };
