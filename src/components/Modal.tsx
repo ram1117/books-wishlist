3e8e806d7ecc5954726c293a1156b1bd3b09d599
ram1117 <ramkumark1@outlook.com>
2023-08-09 22:06:41 +0530

    add types to modal props

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,11 +3,20 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 
 const BOOK_URL = 'https://gutendex.com//books/';
-const Modal = ({ bookId, funcModal }) => {
-  Modal.defaultProps = { bookId: '', funcModal: null };
-  Modal.propTypes = { bookId: PropTypes.string, funcModal: PropTypes.func };
+
+const defaultProps = {
+  bookId: '',
+  funcModal: null,
+};
+
+interface ModalProp {
+  bookId: string;
+  funcModal: Function;
+};
+
+const Modal = ({ bookId, funcModal }: ModalProp) => {
   const [bookData, setBookData] = useState([]);
-  const fetchBookDetails = async (id) => {
+  const fetchBookDetails = async (id: string) => {
     if (id !== '') {
       const response = await fetch(new URL(BOOK_URL + id));
       const bookDetail = await response.json();
@@ -28,21 +37,31 @@ const Modal = ({ bookId, funcModal }) => {
         <div className="popup-wrapper">
           <div className="popup">
             <h1>{bookData.title}</h1>
-            <img className="img-logo" src={bookData.formats['image/jpeg']} alt="book thumbnail" />
+            <img
+              className="img-logo"
+              src={bookData.formats['image/jpeg']}
+              alt="book thumbnail"
+            />
 
             <div className="data-row">
               <p className="column1">Author</p>
               <div className="column2">
-                {bookData.authors.map((author) => <p key={author}>{author.name}</p>)}
+                {bookData.authors.map((author) => (
+                  <p key={author}>{author.name}</p>
+                ))}
               </div>
             </div>
             <div className="data-row">
               <p className="column1">Subject</p>
               <div className="column2">
-                {bookData.subjects.map((subject) => <p key={subject}>{subject}</p>)}
+                {bookData.subjects.map((subject) => (
+                  <p key={subject}>{subject}</p>
+                ))}
               </div>
             </div>
-            <button type="button" onClick={onHandleClick}>Close</button>
+            <button type="button" onClick={onHandleClick}>
+              Close
+            </button>
           </div>
         </div>
       </>
@@ -54,11 +73,14 @@ const Modal = ({ bookId, funcModal }) => {
       <div className="popup-wrapper">
         <div className="popup">
           <h1>{bookData.title}</h1>
-          <button type="button" onClick={onHandleClick}>Close</button>
+          <button type="button" onClick={onHandleClick}>
+            Close
+          </button>
         </div>
       </div>
     </>
   );
 };
 
+Modal.defaultProps = defaultProps;
 export default Modal;
